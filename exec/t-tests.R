9b2aecfca5b7b59e62844304644e87daeeabcaee
andre3322 <andrej.ko3322@gmail.com>
2024-09-30 13:04:07 +0200

    improved Distribution Plots and Plot Function

--- exec/t-tests.R
+++ exec/t-tests.R
@@ -11,39 +11,6 @@
 # R Libraries for t-tests and Wilcoxon tests, rstatix and ggpubr
 
 # -----------------------------------------------------------------------------
-
-# t-tests and wilcox-test on pairwise distribution data
-
-1. Distributions of distances of paralogs and orthologs
-  2. Distribution of mean distances
-  3. Distribution of median distances
-
-4. Distributions of distances per tissue
-  5. Distribution of mean distances per tissue
-  6. Distribution of median distances per tissue
-
-7. Expression angles
-8. Expression versatility
-
-- example t_test_examples.R
-
-a <- rnorm(10000, 100, 5)
-b <- rnorm(10000, 1000, 50)
-summary(a)
-summary(b)
-boxplot(list(a=a, b=b))
-t.test(a, b, alternative='greater')
-t.test(b, a, alternative='greater')
-?t.test
-t.test(b, a, alternative='greater')
-savehistory('~/Desktop/t_test_examples.R')
-
-
-
-
-load("y-data.RData")
-
-# -----------------------------------------------------------------------------
 1. Distributions of distances of paralogs and orthologs
 # -----------------------------------------------------------------------------
 
@@ -65,10 +32,12 @@ ortho_median <- orthologs.exp.prof.dists_stats_df[ ,  c("Family", "median")]
 # calculate mean Values for the Dataframe if row/column Element contain multiple values
 # column_name , column where the mean is calculated
 
-column_name <- "mean"
-df <- para_mean
+column_name <- "median"
+df <- para_median
 
 library(dplyr)
+
+source("exec/t_test_functions.R")
 
 df_means <- calculate_means(df , column_name = column_name)
 
@@ -78,40 +47,80 @@ ortho_mean_df_mean <- df_means
 ortho_mean_df_median <- df_means
 
 # merging the dataframes on which the pairwise analysis is performed
-
-merged_df <- ortho_mean_df_median %>%
-  left_join(para_mean_df_median, by = "Family")
-
-para_ortho_mean_df_mean <- merged_df
-para_ortho_mean_df_median <- merged_df
-
 # performing t-tests and Wilcoxon tests on pairwise distribution data
+# provide the output file name, Achsis labels etc. as arguments to the function
+
+merged_df <- ortho_mean_df_mean %>%
+  left_join(para_mean_df_mean, by = "Family")
+para_ortho_mean_df_mean <- merged_df
 
 data <- para_ortho_mean_df_mean
-
-result <- perform_analysis(data = data, output_file = "results")
-
-
-# provide the output file name, Achsis labels etc. as arguments
-
-data <- para_ortho_mean_df_mean
+output_file_2 <- "2._Distribution_of_mean_distances_(para_ortho)_(t-test_wilcox-test)"
 cols <- colnames(data)
 cols <- cols[cols != "Family"]
-
 data_long <- data %>% pivot_longer(cols = all_of(cols), names_to = "group", values_to = "value")
 
-plot_title <- "Boxplot Comparison"
+source("exec/t_test_functions.R")
+result <- perform_analysis(data_long, output_file_2)
+
+plot_title <- output_file_2
 x_label <- " "
-y_label <- "Mean Expression Profile Distance"
-output_file <- "boxplot_comparison_result.pdf"
+y_label <- "mean_Expression_Profile_Distances"
+custom_x_labels = c(
+  "mean.x" = "Orthologs",
+  "mean.y" = "Paralogs"
+)
+output_file <- file.path("plots", paste0(output_file_2, ".pdf"))
 
 plot <- create_and_save_boxplot(
   data_long = data_long,  
   output_file = output_file,
   plot_title = plot_title,
   x_label = x_label,
-  y_label = y_label
+  y_label = y_label,
+  custom_x_labels = custom_x_labels,
+  point_size = 0.5, 
+  plot_width = 8, 
+  plot_height = 6 
 )
+
+# -----------------------------------------------------------------------------
+
+merged_df <- ortho_mean_df_median %>%
+  left_join(para_mean_df_median, by = "Family")
+para_ortho_mean_df_median <- merged_df
+
+data <- para_ortho_mean_df_median
+output_file_3 <- "3._Distribution_of_median_distances_(para_ortho)_(t-test_wilcox-test)"
+cols <- colnames(data)
+cols <- cols[cols != "Family"]
+data_long <- data %>% pivot_longer(cols = all_of(cols), names_to = "group", values_to = "value")
+
+source("exec/t_test_functions.R")
+result <- perform_analysis(data_long, output_file_3)
+
+plot_title <- output_file_3
+x_label <- " "
+y_label <- "median_Expression_Profile_Distances"
+custom_x_labels = c(
+  "median.x" = "Orthologs",
+  "median.y" = "Paralogs"
+)
+output_file <- file.path("plots", paste0(output_file_3, ".pdf"))
+
+plot <- create_and_save_boxplot(
+  data_long = data_long,  
+  output_file = output_file,
+  plot_title = plot_title,
+  x_label = x_label,
+  y_label = y_label,
+  custom_x_labels = custom_x_labels,
+  point_size = 0.5, 
+  plot_width = 8, 
+  plot_height = 6 
+)
+
+
 
 # ------------------------------------------------------------------------------------------------------
 4. Distributions of distances per tissue
@@ -125,56 +134,102 @@ plot <- create_and_save_boxplot(
 
 # adjusting and selecting the columns of interest for the pairwise analysis
 
-df_ortho_mean <- df_median_mean_paralogs.exp.prof.dists.tissue[ ,  c("Cluster", "Mean_Fat_Body", "Mean_Gut", "Mean_Muscle", "Mean_Whole_Body")]
-df_ortho_median <- df_median_mean_orthologs.exp.prof.dists.tissue[ ,  c("Cluster", "Median_Fat_Body", "Median_Gut", "Median_Muscle", "Median_Whole_Body")]
-df_para_mean <- df_median_mean_paralogs.exp.prof.dists.tissue[ ,  c("Cluster", "Mean_Fat_Body", "Mean_Gut", "Mean_Muscle", "Mean_Whole_Body")]
-df_para_median <- df_median_mean_orthologs.exp.prof.dists.tissue[ ,  c("Cluster", "Median_Fat_Body", "Median_Gut", "Median_Muscle", "Median_Whole_Body")]
+df_ortho_mean_tissue <- df_median_mean_paralogs.exp.prof.dists.tissue[ ,  c("Cluster", "Mean_Fat_Body", "Mean_Gut", "Mean_Muscle", "Mean_Whole_Body")]
+df_ortho_median_tissue <- df_median_mean_orthologs.exp.prof.dists.tissue[ ,  c("Cluster", "Median_Fat_Body", "Median_Gut", "Median_Muscle", "Median_Whole_Body")]
+df_para_mean_tissue <- df_median_mean_paralogs.exp.prof.dists.tissue[ ,  c("Cluster", "Mean_Fat_Body", "Mean_Gut", "Mean_Muscle", "Mean_Whole_Body")]
+df_para_median_tissue <- df_median_mean_orthologs.exp.prof.dists.tissue[ ,  c("Cluster", "Median_Fat_Body", "Median_Gut", "Median_Muscle", "Median_Whole_Body")]
+
 
 # merging the dataframes on which the pairwise analysis is performed
-
-merged_df <- df_ortho_median %>%
-  left_join(df_para_median, by = "Cluster")
-
-df_ortho_para_mean <- merged_df
-df_ortho_para_median <- merged_df
-
-# conerting the dataframes to long format for t-tests and Wilcoxon tests
-
-data <- df_ortho_para_median
-cols <- colnames(data)
-cols <- cols[cols != "Cluster"]
-
-data_long <- data %>%
-  pivot_longer(cols = all_of(cols), 
-               names_to = "group", 
-               values_to = "value")
+# converting the dataframes to long format for t-tests and Wilcoxon tests, and performing Analysis
+# Plotting the t-test results
 
 source("data/exec/t_test_functions.R")
 
-# performing t-tests and Wilcoxon tests on pairwise distribution data
-# stored in the tsv file
+merged_df <- df_ortho_mean_tissue %>%
+  left_join(df_para_mean_tissue, by = "Cluster")
+df_ortho_para_mean_tissue <- merged_df
 
-result <- perform_analysis(data = data, output_file = "results-df_ortho_para_median")
+data <- df_ortho_para_mean_tissue
+cols <- colnames(data)
+cols <- cols[cols != "Cluster"]
+data_long <- data %>% pivot_longer(cols = all_of(cols), names_to = "group", values_to = "value")
+output_file_5 <- "5._Distribution_of_mean_distances_per_tissue_(para_ortho)_(t-test_wilcox-test)"
+result <- perform_analysis(data_long, output_file_5)
 
-# Plotting the t-test results
-
-plot_title <- "Boxplot Comparison"
+plot_title <- output_file_5
 x_label <- " "
-y_label <- "Mean Expression Profile Distance"
-output_file <- "boxplot_comparison_result_jdfnjdfn.pdf"
+y_label <- "mean_Expression_Profile_Distances"
+custom_x_labels = c(
+  "Mean_Fat_Body.x" = "Fat Body (Orthologs)",
+  "Mean_Gut.x" = "Gut (Orthologs)",
+  "Mean_Muscle.x" = "Muscle (Orthologs)",
+  "Mean_Whole_Body.x" = "Whole Body (Orthologs)",
+  "Mean_Fat_Body.y" = "Fat Body (Paralogs)",
+  "Mean_Gut.y" = "Gut (Paralogs)",
+  "Mean_Muscle.y" = "Muscle (Paralogs)",
+  "Mean_Whole_Body.y" = "Whole Body (Paralogs)"
+)
+
+output_file <- file.path("plots", paste0(output_file_5, ".pdf"))
 
 plot <- create_and_save_boxplot(
   data_long = data_long,  
   output_file = output_file,
   plot_title = plot_title,
   x_label = x_label,
-  y_label = y_label
+  y_label = y_label,
+  custom_x_labels = custom_x_labels,
+  point_size = 0.1, 
+  plot_width = 14, 
+  plot_height = 8 
 )
 
 
+# -------------------------------------------------------
+
+merged_df <- df_ortho_median_tissue %>%
+  left_join(df_para_median_tissue, by = "Cluster")
+df_ortho_para_median_tissue <- merged_df
+
+data <- df_ortho_para_median_tissue
+cols <- colnames(data)
+cols <- cols[cols != "Cluster"]
+data_long <- data %>% pivot_longer(cols = all_of(cols), names_to = "group", values_to = "value")
+output_file_6 <- "6._Distribution_of_median_distances_per_tissue_(para_ortho)_(t-test_wilcox-test)"
+result <- perform_analysis(data_long, output_file_6)
+
+plot_title <- output_file_6
+x_label <- " "
+y_label <- "median_Expression_Profile_Distances"
+custom_x_labels = c(
+  "Median_Fat_Body.x" = "Fat Body (Orthologs)",
+  "Median_Gut.x" = "Gut (Orthologs)",
+  "Median_Muscle.x" = "Muscle (Orthologs)",
+  "Median_Whole_Body.x" = "Whole Body (Orthologs)",
+  "Median_Fat_Body.y" = "Fat Body (Paralogs)",
+  "Median_Gut.y" = "Gut (Paralogs)",
+  "Median_Muscle.y" = "Muscle (Paralogs)",
+  "Median_Whole_Body.y" = "Whole Body (Paralogs)"
+)
+
+output_file <- file.path("plots", paste0(output_file_6, ".pdf"))
+
+plot <- create_and_save_boxplot(
+  data_long = data_long,  
+  output_file = output_file,
+  plot_title = plot_title,
+  x_label = x_label,
+  y_label = y_label,
+  custom_x_labels = custom_x_labels,
+  point_size = 0.1, 
+  plot_width = 14, 
+  plot_height = 8 
+)
+
 # ------------------------------------------------------------------------------------------------------
-4. Distributions of distances per tissue
+7. Distributions of Angles
 # ------------------------------------------------------------------------------------------------------
 
-7. Expression angles
-8. Expression versatility
+8. Expression angles
+9 Expression versatility
--- exec/t_test_functions.R
+++ exec/t_test_functions.R
@@ -32,7 +32,7 @@ calculate_means <- function(df, column_name) {
 
 # Input: data (Dataframe) and output_file (Output file name)
 
-perform_analysis <- function(data, output_file) {
+perform_analysis <- function(data_long, output_file) {
 
   library(ggplot2)
   library(tidyr)
@@ -40,30 +40,26 @@ perform_analysis <- function(data, output_file) {
   library(rstatix)
   library(ggpubr)  
   
+  # Perform t-test and Wilcoxon test
   ttest_result <- t_test(value ~ group, data = data_long, paired = TRUE)
   wilcox_result <- wilcox_test(value ~ group, data = data_long, paired = TRUE)
   
+  # Adjust p-values using Benjamini-Hochberg (BH) method
   p_vals <- c(ttest_result$p, wilcox_result$p)
   adjusted_p_vals <- p.adjust(p_vals, method = "BH")
   
+  # Append adjusted p-values
   ttest_result <- ttest_result %>% mutate(adjusted_p = adjusted_p_vals[1:n()])
   wilcox_result <- wilcox_result %>% mutate(adjusted_p = adjusted_p_vals[(n() + 1):(2 * n())])
   
+  # Combine results
   combined_results <- bind_rows(ttest_result, wilcox_result)
-
-  write.table(combined_results, file = paste0(output_file, ".tsv"), sep = "\t", row.names = FALSE, quote = FALSE)
-
-  ggboxplot(data_long, x = "group", y = "value", add = "jitter") +
-    stat_compare_means(method = "t.test", paired = TRUE, label = "p.signif") +
-    stat_compare_means(method = "wilcox.test", paired = TRUE, label = "p.signif")
   
-  return(list(results = combined_results, plot = plot))
-}
+  # Define file path and write output to .tsv file
+  write.table(combined_results, file = file.path("plots", paste0(output_file, ".tsv")), sep = "\t", row.names = FALSE, quote = FALSE)
 
-# Usage:
-# result <- perform_analysis(your_dataframe, output_file = "results.txt")
-# print(result$results)
-# print(result$plot)
+  return(list(results = combined_results))
+}
 
 # -----------------------------------------------------------------------------
 
@@ -72,46 +68,53 @@ perform_analysis <- function(data, output_file) {
 # Input: data_long (Dataframe) and output_file (Output file name)
 
 create_and_save_boxplot <- function(data_long, output_file = "boxplot_comparison.pdf",
-                                    plot_title = NULL, x_label = NULL, y_label = NULL) {
+                                    plot_title = NULL, x_label = NULL, y_label = NULL,
+                                    custom_x_labels = NULL, point_size = 0.3,  # Reduced size for invisibility
+                                    point_alpha = 0.3,  # Increase transparency
+                                    plot_width = 14, plot_height = 8) {
   library(ggplot2)
   library(ggpubr)
 
-  plot <- ggboxplot(data_long, x = "group", y = "value", fill = "group", add = "jitter") +
-    geom_boxplot(color = "black", outlier.shape = 21, outlier.color = "red", outlier.fill = "red") +
+  # Create the 'plots' directory if it doesn't exist
+  if (!dir.exists("plots")) {
+    dir.create("plots")
+  }
+
+  # Ensure 'group' is a factor for proper coloring
+  data_long$group <- as.factor(data_long$group)
+
+  # Create the boxplot with larger elements
+  plot <- ggboxplot(data_long, x = "group", y = "value", fill = "group", 
+                    add = "jitter", add.params = list(size = point_size, alpha = point_alpha)) +  # Adjust size and alpha
     scale_fill_brewer(palette = "Pastel1") +
-    theme_minimal() +
-    scale_y_continuous(limits = c(0, 0.5), breaks = seq(0, 0.5, by = 0.05)) +
+    theme_minimal(base_size = 14) +  # Base size increased for all text elements
+    scale_y_continuous(limits = c(min(data_long$value), max(data_long$value)), breaks = seq(0, 0.5, by = 0.05)) +
     theme(
-      plot.title = element_text(size = 14, face = "bold", margin = margin(t = 20, b = 20), hjust = 0.40),
-      axis.title.x = element_text(size = 14, face = "bold", margin = margin(t = 20), hjust = 0.32),
-      axis.title.y = element_text(size = 14, margin = margin(r = 20)),
-      axis.text.x = element_text(size = 10, angle = 35, hjust = 0.5),
+      plot.title = element_text(size = 13, face = "bold", margin = margin(t = 20, r = 0 ,b = 20, l = 0), hjust = 0.40),
+      axis.title.x = element_text(size = 12, face = "bold", margin = margin(t = 20, r = 0, b = 20, l = 0), hjust = 0.32),
+      axis.title.y = element_text(size = 12, face = "bold", margin = margin(t = 20, r = 20, b = 20, l = 20)),
+      axis.text.x = element_text(size = 10, angle = 35, hjust = 0.5, vjust = 0.5),
       axis.text.y = element_text(size = 10),
+      plot.margin = margin(t = 10, r = 30, b = 10, l = 10),
       panel.grid.major = element_line(color = "gray80"),
       legend.position = "none"
     ) +
-    stat_compare_means(method = "t.test", paired = TRUE, label = "p.format") +
-    stat_compare_means(method = "wilcox.test", paired = TRUE, label = "p.format", label.y = 0.45)
+    # Increased label size and color for statistical comparisons
+    stat_compare_means(method = "t.test", paired = TRUE, label = "p.signif", 
+                      label.y = max(data_long$value) * 0.9, size = 10, color = "blue") +
+    stat_compare_means(method = "wilcox.test", paired = TRUE, label = "p.signif", 
+                      label.y = max(data_long$value) * 0.75, size = 10, color = "red")
 
+  # Add optional titles and labels
   if (!is.null(plot_title)) plot <- plot + ggtitle(plot_title)
   if (!is.null(x_label)) plot <- plot + xlab(x_label)
   if (!is.null(y_label)) plot <- plot + ylab(y_label)
+  if (!is.null(custom_x_labels)) {
+    plot <- plot + scale_x_discrete(labels = custom_x_labels)
+  }
 
-  ggsave(filename = output_file, plot = plot, device = "pdf", width = 8, height = 6)
- 
+  # Save the plot to a larger PDF with increased dimensions
+  ggsave(filename = file.path("plots", output_file), plot = plot, device = "pdf", width = plot_width, height = plot_height)
+
   return(plot)
 }
-
-# Usage:
-
-#data <- para_ortho_mean_df_mean
-#cols <- colnames(data)
-#cols <- cols[cols != "Family"]
-
-#plot <- create_and_save_boxplot(
-#  data_long = data_long,  
-#  output_file = output_file,
-#  plot_title = plot_title,
-#  x_label = x_label,
-#  y_label = y_label
-#)
