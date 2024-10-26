581547a6b677f3cd3fc72ed3bf416f52abc469d2
andre3322 <andrej.ko3322@gmail.com>
2024-10-26 21:54:12 +0200

    added t-tests to rscripts

--- exec/plot_exp.prof.dists_distribution.R
+++ exec/plot_exp.prof.dists_distribution.R
@@ -16,14 +16,6 @@ results_dir <- Sys.getenv("RESULTS_DIR")
 load(file.path(output_data_dir, "expression_profile_distances_statistics.RData"))
 load(file.path(output_data_dir, "ExpressionProfileDistances.RData"))
 
-# Function to determine the level of significance
-significance_level <- function(p) {
-  if (p < 0.001) return("***")
-  else if (p < 0.01) return("**")
-  else if (p < 0.05) return("*")
-  else return("ns")  
-}
-
 # mean and median expression distances
 # df_median_mean_paralogs 
 # df_median_mean_orthologs
@@ -36,126 +28,79 @@ ortholog.median.lst <- split(df_median_mean_orthologs$Median, df_median_mean_ort
 
 
 # basically create df from the lists, beneath each other in the df, regardless of df length
-df_mean.dists <- map_dfr(list(Ortholog = ortholog.mean.lst, Paralog = paralog.mean.lst), 
-              ~tibble(Cluster = names(.x), Distance = unlist(.x)), .id = "Type")
+df_mean.dists <- map_dfr(list(Ortholog = ortholog.mean.lst, Paralog = paralog.mean.lst), ~tibble(Cluster = names(.x), Distance = unlist(.x)), .id = "Type")
 
-df_median.dists <- map_dfr(list(Ortholog = ortholog.median.lst, Paralog = paralog.median.lst), 
-              ~tibble(Cluster = names(.x), Distance = unlist(.x)), .id = "Type")
+df_median.dists <- map_dfr(list(Ortholog = ortholog.median.lst, Paralog = paralog.median.lst), ~tibble(Cluster = names(.x), Distance = unlist(.x)), .id = "Type")
 
 
-# Create a boxplot for mean distances
+# ---------------------------------------------------------------------------
+
+# t-tests pairwise distribution data in long format (already in long format)
+
+# Function to determine the level of significance
+significance_level <- function(p) {
+  if (p < 0.001) return("***")
+  else if (p < 0.01) return("**")
+  else if (p < 0.05) return("*")
+  else return("ns")  
+}
+
+# Perform t-test for median and adjust p-values, then apply the significance_level function
+
+t_test_median <- df_median.dists %>%
+  t_test(Distance ~ Type, alternative = "greater") %>% 
+  adjust_pvalue(method = "BH") %>%                    
+  mutate(significance = sapply(p, significance_level),
+         analysis = "Median")  
+
+t_test_mean <- df_median.dists %>%
+  t_test(Distance ~ Type, alternative = "greater") %>% 
+  adjust_pvalue(method = "BH") %>%                    
+  mutate(significance = sapply(p, significance_level),
+         analysis = "Mean")  
+
+# Combine the two results into one summary dataframe
+t_test_summary <- bind_rows(t_test_median, t_test_mean)
+
+write.csv(t_test_summary, file.path(results_dir, "t_test_summary.csv"), row.names = FALSE)
+
+# -----------------------------------------------------------------------------
+
+# Extract the significance levels are already in the boxplot
+
+
+# Create the boxplot with the significance annotation
 boxplot_mean <- ggplot(df_mean.dists, aes(x = Type, y = Distance, fill = Type)) +
   geom_boxplot(outlier.shape = NA) +
   geom_jitter(width = 0.1, alpha = 0.3, size = 1) +
   labs(title = "Mean Expression Distances", y = "Distance") +
   theme_minimal() +
   geom_signif(comparisons = list(c("Ortholog", "Paralog")), map_signif_level = TRUE) +
-  theme(plot.title = element_text(size = 12, face = "bold", margin = margin(t = 20, r = 0 ,b = 20, l = 0), hjust = 0.50),
-        axis.title.x = element_text(size = 10, margin = margin(t = 20, r = 0, b = 20, l = 0), hjust = 0.50),
-        axis.title.y = element_text(size = 10, margin = margin(t = 20, r = 20, b = 20, l = 20)),
+  theme(
+    plot.title = element_text(size = 12, face = "bold", margin = margin(t = 20, r = 0 ,b = 20, l = 0), hjust = 0.50),
+    axis.title.x = element_text(size = 10, margin = margin(t = 20, r = 0, b = 20, l = 0), hjust = 0.50),
+    axis.title.y = element_text(size = 10, margin = margin(t = 20, r = 20, b = 20, l = 20))
   )
 
-# Create a boxplot for median distances
+# Create the boxplot with the significance annotation for median distances
 boxplot_median <- ggplot(df_median.dists, aes(x = Type, y = Distance, fill = Type)) +
-  geom_boxplot(outlier.shape = NA) +  # Exclude outliers from the boxplot to avoid overlap
-  geom_jitter(width = 0.1, alpha = 0.3, size = 1) +  # Add jittered points
+  geom_boxplot(outlier.shape = NA) +
+  geom_jitter(width = 0.1, alpha = 0.3, size = 1) +
   labs(title = "Median Expression Distances", y = "Distance") +
   theme_minimal() +
-  geom_signif(comparisons = list(c("Orthologs", "Paralogs")), map_signif_level = TRUE) +
-  theme(plot.title = element_text(size = 12, face = "bold", margin = margin(t = 20, r = 0 ,b = 20, l = 0), hjust = 0.50),
-        axis.title.x = element_text(size = 10, margin = margin(t = 20, r = 0, b = 20, l = 0), hjust = 0.50),
-        axis.title.y = element_text(size = 10, margin = margin(t = 20, r = 20, b = 20, l = 20)),
+  geom_signif(comparisons = list(c("Ortholog", "Paralog")), map_signif_level = TRUE) +
+  theme(
+    plot.title = element_text(size = 12, face = "bold", margin = margin(t = 20, r = 0 ,b = 20, l = 0), hjust = 0.50),
+    axis.title.x = element_text(size = 10, margin = margin(t = 20, r = 0, b = 20, l = 0), hjust = 0.50),
+    axis.title.y = element_text(size = 10, margin = margin(t = 20, r = 20, b = 20, l = 20))
   )
 
-# Save all three boxplots in a single PDF
-pdf(file.path(results_dir,"boxplots_expression_distances_with_jitter.pdf"), height = 15, width = 7)
+# Save the two Boxplots in a single PDF
+pdf(file.path(results_dir,"boxplots_expression_distances_with_jitter11.pdf"), height = 15, width = 7)
 grid.arrange(boxplot_median, boxplot_mean, ncol = 1)
 dev.off()
 
 
 
 
-
-# t-tests
-# # Uncomment this part to perform t-test for medians
-# t_test_medians <- t.test(distance ~ group, data = median_distances, alternative = 'greater')
-
-# Uncomment this part to perform t-test for means
-# t_test_mean_results <- t.test(distance ~ group, data = mean_distances, alternative = 'greater')
-
-# Uncomment this part to save the t-test results in a dataframe
-# t_test_summary <- data.frame(
-#   analysis = c("Median", "Mean"),
-
-  
-#   # Results without correction
-#   statistic_original = c(
-#                           t_test_medians$statistic, 
-#                           t_test_mean_results$statistic),
-#   p_value_original = c(
-#                        t_test_medians$p.value, 
-#                        t_test_mean_results$p.value),
-#   conf_int_lower_original = c(
-#                                t_test_medians$conf.int[1], 
-#                                t_test_mean_results$conf.int[1]),
-#   conf_int_upper_original = c(
-#                                t_test_medians$conf.int[2], 
-#                                t_test_mean_results$conf.int[2]),
-#   # Use the means reported by the t-tests
-#   mean_orthologs = c(
-#                       t_test_medians$estimate[1],     
-#                       t_test_mean_results$estimate[1]),  
-#   mean_paralogs = c(
-#                      t_test_medians$estimate[2],     
-#                      t_test_mean_results$estimate[2]), 
-  
-#   # Significance
-#   significance_original = c(
-#                     significance_level(t_test_medians$p.value),
-#                     significance_level(t_test_mean_results$p.value)),
-#   p_value_corrected = p.adjust(c(
-#                                   t_test_medians$p.value, 
-#                                   t_test_mean_results$p.value), 
-#                                 method = "BH")
-# )
-
-# # Add a column of significance based on the corrected p-value
-# t_test_summary$significance_corrected <- sapply(t_test_summary$p_value_corrected, significance_level)
-
-# # Uncomment this part to save the original and corrected t-test results to a CSV file
-# write.csv(t_test_summary, file.path(results_dir, "t_test_results_original_and_corrected.csv"), row.names = FALSE)
-
-
-
-
-# -----------------------------------------------------------------------------
-
-# t-tests and plots based on:
-# https://www.datanovia.com/en/fr/blog/comment-effectuer-un-test-t-multiple-dans-r-pour-differentes-variables/
-# located in folder: plots/t-test-plots
-
-library(tidyverse)
-library(rstatix)
-library(ggpubr)
-library(tibble)
-
-
-
-# --------------------------------------------------------
-
-
-mydata <- p.df %>% as_tibble()
-
-mydata.long <- mydata %>% pivot_longer(-gene.type, names_to = "variables", values_to = "value")
-mydata.long <- mydata.long %>% filter(!is.na(value) & !is.infinite(value))
-
-stat.test <- mydata.long %>%
-  group_by(variables) %>%  
-  t_test(value ~ gene.type) %>%  
-  adjust_pvalue(method = "BH") %>%  
-  add_significance()
-
-# Add p-values to the plot
-stat.test <- stat.test %>% add_xy_position(x = "gene.type") 
-myplot_with_pvals <- myplot + stat_pvalue_manual(stat.test, label = "p.adj.signif")
 
--- exec/plot_exp.prof.dists_distribution_tissue.R
+++ exec/plot_exp.prof.dists_distribution_tissue.R
@@ -25,6 +25,9 @@ load("data/data.RData")
 load(file.path(output_data_dir, "/gene_family_expression_dists.RData"))
 load(file.path(output_data_dir, "/boxplot_expression_distances.RData"))
 
+
+load(file.path(output_data_dir, "expression_distances_tissue.RData"))
+
 significance_level <- function(p) {
   if (p < 0.001) return("***")
   else if (p < 0.01) return("**")
@@ -32,10 +35,35 @@ significance_level <- function(p) {
   else return("ns")  
 }
 
+# mean_p.df_long
+# median_p.df_long
 
-load(file.path(output_data_dir, "expression_distances_tissue.RData"))
+# Perform t-tests for median and mean per tissue between orthologs and paralogs group 
+# and adjust p-values, then apply the significance_level function
+# group_by() -> defines the tissue column
 
-# Plotting process
+t_test_median_tissue <- median_p.df_long %>%
+  group_by(variables) %>%                             
+  t_test(value ~ Type, alternative = "greater") %>%  
+  adjust_pvalue(method = "BH") %>%                   
+  mutate(significance = sapply(p, significance_level),  
+         analysis = "Median")  
+
+t_test_mean_tissue <- mean_p.df_long %>%
+  group_by(variables) %>%                             
+  t_test(value ~ Type, alternative = "greater") %>%  
+  adjust_pvalue(method = "BH") %>%                   
+  mutate(significance = sapply(p, significance_level),  
+         analysis = "Mean")  
+
+t_test_summary_tissue <- bind_rows(t_test_median_tissue, t_test_mean_tissue)
+
+write.csv(t_test_summary_tissue, file.path(results_dir, "t_test_summary_tissue.csv"), row.names = FALSE)
+
+# ---------------------------------------------------------------------------
+
+# geom_signif(): This function is used to add significance annotations directly to the plot 
+# so plots already have the significance level !!
 
 # Creating a Boxplot for Mean Distances
 # Calculate the appropriate height for the PDF based on the number of facets
@@ -47,7 +75,7 @@ boxplot_mean <- ggplot(mean_p.df_long, aes(x = Type, y = value, fill = Type)) +
   geom_jitter(width = 0.1, alpha = 0.3, size = 1) +
   labs(title = "Mean Expression Distances Tissue", y = "Distance") +
   theme_pubr(border = TRUE) +
-  geom_signif(comparisons = list(c("Ortholog", "Paralog")), map_signif_level = TRUE) +
+  geom_signif(comparisons = list(c("ortholog", "paralog")), map_signif_level = TRUE) +
   theme(plot.title = element_text(size = 12, face = "bold", margin = margin(t = 20, r = 0 ,b = 20, l = 0), hjust = 0.50),
         axis.title.x = element_text(size = 10, margin = margin(t = 20, r = 0, b = 20, l = 0), hjust = 0.50),
         axis.title.y = element_text(size = 10, margin = margin(t = 20, r = 20, b = 20, l = 20))) +
@@ -56,12 +84,14 @@ boxplot_mean <- ggplot(mean_p.df_long, aes(x = Type, y = value, fill = Type)) +
 ggsave(file.path(results_dir, "tissues_mean_boxplot12.pdf"), plot = boxplot_mean, width = 12, height = plot_height)
 
 
+
+
 # Creating a Boxplot for Median Distances
 # Calculate the appropriate height for the PDF based on the number of facets
 n_facets <- length(unique(median_p.df_long$variables))
 plot_height <- 8 * n_facets  # 8 inches per facet
 
-boxplot_mean <- ggplot(median_p.df_long, aes(x = Type, y = value, fill = Type)) +
+boxplot_median <- ggplot(median_p.df_long, aes(x = Type, y = value, fill = Type)) +
   geom_boxplot(outlier.shape = NA) +
   geom_jitter(width = 0.1, alpha = 0.3, size = 1) +
   labs(title = "Median Expression Distances Tissue", y = "Distance") +
@@ -72,93 +102,5 @@ boxplot_mean <- ggplot(median_p.df_long, aes(x = Type, y = value, fill = Type)) 
         axis.title.y = element_text(size = 10, margin = margin(t = 20, r = 20, b = 20, l = 20))) +
   facet_grid(variables ~ ., scales = "free_y", space = "free_y")
 
-ggsave(file.path(results_dir, "tissues_median_boxplot12.pdf"), plot = boxplot_mean, width = 12, height = plot_height)
+ggsave(file.path(results_dir, "tissues_median_boxplot12.pdf"), plot = boxplot_median, width = 12, height = plot_height)
 
-
-
-# ------------------------------------------------------------------------------------
-
-
-
-# Perform the t-test and save the p-values
-
-t_test_results <- filtered_data %>% filter(!is.na(distance)) %>%
-  group_by(tissue) %>%
-  summarise(
-    t_test = list(t.test(distance ~ type, data = ., alternative = 'greater')),
-    .groups = "drop"  # To avoid warnings
-  ) %>%
-  ungroup() %>%
-  mutate(
-    p_value = map_dbl(t_test, ~ .x$p.value),        # Extract the p-value from the t-test
-    statistic = map_dbl(t_test, ~ .x$statistic)     # Extract the test statistic from the t-test
-  ) %>%
-  select(-t_test)  # Remove the column with t-test objects to clean up results
-
-# Apply p-value correction
-t_test_results <- t_test_results %>%
-  mutate(
-    p_value_adjusted = p.adjust(p_value, method = "BH"), 
-    significance_original = sapply(p_value, significance_level), 
-    significance_adjusted = sapply(p_value_adjusted, significance_level)  
-  )
-
-write.csv(t_test_results, results_dir+"/t_test_results_by_tissue.csv", row.names = FALSE)
-
-# Check that both groups are present
-p <- ggplot(filtered_data, aes(x = tissue, y = distance, fill = type)) +
-  geom_boxplot() +
-  labs(title = "Expression Distances by Tissue",
-       x = "Tissue",
-       y = "Distance") +
-  scale_fill_manual(values = c("Orthologs" = "blue", "Paralogs" = "red")) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-for (i in 1:nrow(t_test_results)) {
-  p <- p + annotate("text", 
-                    x = i,  
-                    y = max(filtered_data$distance, na.rm = TRUE) + 0.02, 
-                    label = t_test_results$significance_adjusted[i], 
-                    size = 5, 
-                    color = "black")  
-}
-
-pdf(results_dir+"/boxplot_expression_distances_by_tissue.pdf", height = 15, width = 7)
-print(p)
-dev.off()
-
-
-
-
-
-# ------------------------------------------------------------------------------------
-
-
-
-# Perform Wilcoxon test for each tissue (variables) across gene types
-stat.test <- mydata_long %>% 
-  group_by(variables) %>% 
-  wilcox_test(value ~ gene.type, p.adjust.method = "BH") %>% 
-  add_significance()
-
-# Create the boxplot
-myplot <- ggboxplot(
-  mydata_long,
-  x = "gene.type",
-  y = "value",
-  fill = "gene.type",
-  palette = "npg",  
-  legend = "none",
-  ggtheme = theme_pubr(border = TRUE)
-) + facet_wrap(~variables)
-
-# Add p-values to the plot
-stat.test <- stat.test %>% add_xy_position(x = "gene.type")
-myplot_with_pvals <- myplot + stat_pvalue_manual(stat.test, label = "p.adj.signif")
-
-# Save the plot as a PDF
-ggsave("plots/wilcox-test-plots/tissues_mean_boxplot_with_pvalues.pdf", plot = myplot_with_pvals, width = 12, height = 8)
-
-# Save the statistical results as a .tsv file
-write_tsv(stat.test, "plots/wilcox-test-plots/tissues_mean_statistical_results.tsv")
